74188d57399187c21802427537d5f21fb90fdf03
Jacob Kelderman <jkeld@iastate.edu>
2022-08-04 20:06:59 -0500

    random changes to keep updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello Welcome To My Website!</h1>
+        <p>This site is still in the works. In the meantime check out my <a href = "https://www.linkedin.com/in/jacob-kelderman-72ba671a7/">LinkedIn</a></p>
       </header>
     </div>
   );
